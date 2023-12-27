a4e29bf4bc839e02a1ed50fc3f4e182b960a1dff
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-12-27 19:28:56 +0700

    add outlined textfield for nama in FormInputSiswa

--- app/src/main/java/com/example/kontak/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/kontak/ui/home/screen/InsertScreen.kt
@@ -2,7 +2,10 @@ package com.example.kontak.ui.home.screen
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -20,6 +23,13 @@ fun FormInputSiswa(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-
+        OutlinedTextField(
+            value = insertUiEvent.nama,
+            onValueChange ={onValueChange(insertUiEvent.copy(nama = it))},
+            label = { Text("Nama") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled
+        )
     }
 }
