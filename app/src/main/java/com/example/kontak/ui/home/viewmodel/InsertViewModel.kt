a7395d8f3482f12e95bbeffa47f5f9f461ac5161
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-12-27 19:19:19 +0700

    Create function for InsertUiEvent and InsertUiState

--- app/src/main/java/com/example/kontak/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/kontak/ui/home/viewmodel/InsertViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.kontak.ui.home.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.kontak.model.Kontak
 import com.example.kontak.repository.KontakRepository
 
 class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
@@ -14,7 +15,25 @@ data class InsertUiEvent(
     val nohp: String = ""
 )
 
+fun InsertUiEvent.toKontak() : Kontak = Kontak(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    nohp = nohp,
+)
+
 
 data class InsertUiState(
     val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
+
+fun Kontak.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    nohp = nohp
+)
+
+fun Kontak.toUiStateKontak(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
 )
