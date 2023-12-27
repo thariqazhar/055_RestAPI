f0a3eb9de1d0f85a33fec5ee370152a9fc5c2110
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-12-28 05:21:53 +0700

    add outlined textfield for telepon in FormInputSiswa

--- app/src/main/java/com/example/kontak/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/kontak/ui/home/screen/InsertScreen.kt
@@ -3,11 +3,13 @@ package com.example.kontak.ui.home.screen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.kontak.ui.home.viewmodel.InsertUiEvent
 
@@ -35,6 +37,15 @@ fun FormInputSiswa(
             value = insertUiEvent.alamat,
             onValueChange ={onValueChange(insertUiEvent.copy(alamat = it))},
             label = { Text("Email") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled
+        )
+        OutlinedTextField(
+            value = insertUiEvent.nohp,
+            onValueChange ={onValueChange(insertUiEvent.copy(nohp = it))},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text("Telepon") },
             singleLine = true,
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled
