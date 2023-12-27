f931388752ee6b7277b5fe861a3e9e90314866fd
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-12-27 19:16:34 +0700

    Create data class InsertUiEvent

--- app/src/main/java/com/example/kontak/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/kontak/ui/home/viewmodel/InsertViewModel.kt
@@ -1,4 +1,15 @@
 package com.example.kontak.ui.home.viewmodel
 
-class InsertViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.kontak.repository.KontakRepository
+
+class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
+
 }
+
+data class InsertUiEvent(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val nohp: String = ""
+)
