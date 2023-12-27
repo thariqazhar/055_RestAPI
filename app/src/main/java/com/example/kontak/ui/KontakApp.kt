d25a55aa4b8b4968ad49d9d10c8780156fcca058
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-12-28 06:47:10 +0700

    Setting the TopAppBar center aligned

--- app/src/main/java/com/example/kontak/ui/KontakApp.kt
+++ app/src/main/java/com/example/kontak/ui/KontakApp.kt
@@ -2,8 +2,12 @@ package com.example.kontak.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -51,7 +55,21 @@ fun TopAppBarKontak(
     scrollBehavior: TopAppBarScrollBehavior? = null,
     navigateUp: () -> Unit
 ) {
-
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Back"
+                    )
+                }
+            }
+        }
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
