b843fdd4fad8fbd2d2c174d0ea8f3bffcd5f87be
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-12-27 19:23:12 +0700

    create var insertKontakState, function updateInsertKontakState, and suspend function insertKontak

--- app/src/main/java/com/example/kontak/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/kontak/ui/home/viewmodel/InsertViewModel.kt
@@ -1,11 +1,31 @@
 package com.example.kontak.ui.home.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.kontak.model.Kontak
 import com.example.kontak.repository.KontakRepository
+import kotlinx.coroutines.launch
 
 class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
 
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent) {
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertKontak() {
+        viewModelScope.launch {
+            try {
+                kontakRepository.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
 }
 
 data class InsertUiEvent(
