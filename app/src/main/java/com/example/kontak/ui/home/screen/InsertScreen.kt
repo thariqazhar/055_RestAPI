6afca8eac85b50a42f29f3f37b9ac98a1580d8a2
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-12-27 19:27:05 +0700

    create function FormInputSiswa in InsertScreen.kt

---
+++ app/src/main/java/com/example/kontak/ui/home/screen/InsertScreen.kt
@@ -0,0 +1,25 @@
+package com.example.kontak.ui.home.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.kontak.ui.home.viewmodel.InsertUiEvent
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputSiswa(
+    insertUiEvent: InsertUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertUiEvent) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+
+    }
+}
