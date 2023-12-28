cff35732975f35e133e4cf90d50cf7be3585d537
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-12-28 08:07:14 +0700

    Add object DestinasiEntry

--- app/src/main/java/com/example/kontak/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/kontak/ui/home/screen/InsertScreen.kt
@@ -15,8 +15,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import com.example.kontak.navigation.DestinasiNavigasi
 import com.example.kontak.ui.home.viewmodel.InsertUiEvent
 import com.example.kontak.ui.home.viewmodel.InsertUiState
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Siswa"
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
