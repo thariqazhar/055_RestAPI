baa22f57a7536c10818d4a7c641cf50d099ad57a
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-12-28 07:16:53 +0700

    Add val for scroll behavior

--- app/src/main/java/com/example/kontak/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/kontak/ui/home/screen/HomeScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -50,7 +51,7 @@ fun HomeScreen(
     onDetailClick: (Int) -> Unit = {},
     viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
-
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 }
 
 @Composable
