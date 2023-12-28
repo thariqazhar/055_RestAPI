550ecacad5e1737f70518560288c9b30ad5487db
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-12-28 07:13:58 +0700

    Create object DestinasiHome

--- app/src/main/java/com/example/kontak/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/kontak/ui/home/screen/HomeScreen.kt
@@ -30,7 +30,13 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.kontak.R
 import com.example.kontak.model.Kontak
+import com.example.kontak.navigation.DestinasiNavigasi
 import com.example.kontak.ui.home.viewmodel.KontakUIState
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Kontak"
+}
 
 @Composable
 fun HomeStatus(
