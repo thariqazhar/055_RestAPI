7150bb253db13de4dfa17e2e447e073763832e2e
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2023-12-28 07:15:06 +0700

    Create function HomeScreen

--- app/src/main/java/com/example/kontak/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/kontak/ui/home/screen/HomeScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -28,14 +29,28 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.kontak.R
 import com.example.kontak.model.Kontak
 import com.example.kontak.navigation.DestinasiNavigasi
+import com.example.kontak.ui.PenyediaViewModel
+import com.example.kontak.ui.home.viewmodel.HomeViewModel
 import com.example.kontak.ui.home.viewmodel.KontakUIState
 
 object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
     override val titleRes = "Kontak"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Int) -> Unit = {},
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
 }
 
 @Composable
